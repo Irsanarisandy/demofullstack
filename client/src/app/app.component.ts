26d48a6dbfc74b53c63585f5b6fa3b61a7ff546e
Irsan Arisandy <irsanarisandy@hotmail.com>
2020-10-16 00:32:41 +1100

    Adding HTTP request on client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  public title = 'The Dating App';
+  public users: any;
+
+  constructor(public http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  private getUsers(): void {
+    this.http.get('https://localhost:5001/api/users').subscribe(
+      response => {
+        this.users = response;
+      }, error => {
+        console.error(error);
+      }
+    );
+  }
 }
