9979aa06cba14ab0b2bff119735b90196697cd4b
Irsan Arisandy <irsanarisandy@hotmail.com>
2020-10-16 02:01:26 +1100

    Adding Angular materials and flex-layout

---
+++ client/src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { SharedMaterialModule } from './shared-material.module';
+
+const allModules = [
+  CommonModule,
+  HttpClientModule,
+  BrowserModule,
+  BrowserAnimationsModule,
+  FlexLayoutModule,
+  SharedMaterialModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: allModules,
+  exports: allModules
+})
+export class SharedModule { }
