f878331975116b7a0fb9f9f3d56852d4e254acde
Irsan Arisandy <irsanarisandy@hotmail.com>
2020-10-19 00:08:46 +1100

    Add footer

--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 
 import { LayoutComponent } from './layout.component';
-import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { RegisterComponent } from './components/register/register.component';
 
 import { LayoutRoutingModule } from './layout-routing.module';
@@ -11,8 +12,9 @@ import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [
     LayoutComponent,
-    NavComponent,
     HomeComponent,
+    NavComponent,
+    FooterComponent,
     RegisterComponent
   ],
   imports: [
